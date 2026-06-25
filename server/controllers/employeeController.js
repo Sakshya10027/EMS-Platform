22d3e9ab81046e07dfe5dc33061a8bcf75d0da2a
Sakshya10027 <patelsakshya2@gmail.com>
2026-06-25 22:51:03 +0530

    Employee and User model added

---
+++ server/controllers/employeeController.js
@@ -0,0 +1,13 @@
+// Get employees
+// GET /api/employees
+
+export const getEmployees = async (req, res) => {
+
+}
+
+// create employee
+// POST /api/employee
+
+export const createEmployees = async (req, res) => {
+
+}
